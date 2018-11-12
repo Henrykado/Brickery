9b8cee45f1821acb6113240c983c80504ba7e0b0
bee <42713053+beeshroom@users.noreply.github.com>
2018-11-12 03:44:48 -0500

    Add files via upload

--- java/com/beeshroom/Brickery/init/ModBlocks.java
+++ java/com/beeshroom/Brickery/init/ModBlocks.java
@@ -3,30 +3,24 @@ package com.beeshroom.Brickery.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.beeshroom.Brickery.blocks.block_black_terra_brick;
-import com.beeshroom.Brickery.blocks.block_blue_terra_brick;
+import com.beeshroom.Brickery.blocks.BlockTerracottaBrick;
 import com.beeshroom.Brickery.blocks.block_brick_tile;
-import com.beeshroom.Brickery.blocks.block_brown_terra_brick;
-import com.beeshroom.Brickery.blocks.block_cyan_terra_brick;
-import com.beeshroom.Brickery.blocks.block_gray_terra_brick;
-import com.beeshroom.Brickery.blocks.block_green_terra_brick;
-import com.beeshroom.Brickery.blocks.block_light_blue_terra_brick;
-import com.beeshroom.Brickery.blocks.block_light_gray_terra_brick;
-import com.beeshroom.Brickery.blocks.block_lime_terra_brick;
-import com.beeshroom.Brickery.blocks.block_magenta_terra_brick;
-import com.beeshroom.Brickery.blocks.block_orange_terra_brick;
-import com.beeshroom.Brickery.blocks.block_pink_terra_brick;
-import com.beeshroom.Brickery.blocks.block_purple_terra_brick;
-import com.beeshroom.Brickery.blocks.block_red_terra_brick;
-import com.beeshroom.Brickery.blocks.block_terra_brick;
-import com.beeshroom.Brickery.blocks.block_white_terra_brick;
-import com.beeshroom.Brickery.blocks.block_yellow_terra_brick;
+import com.beeshroom.Brickery.blocks.block_brick_wall;
+import com.beeshroom.Brickery.blocks.block_dirty_bricks;
 import com.beeshroom.Brickery.blocks.blockcrackedbricks;
 import com.beeshroom.Brickery.blocks.blockmixedcobble;
 import com.beeshroom.Brickery.blocks.blockmossybricks;
+import com.beeshroom.Brickery.blocks.plank_fancy_acacia;
+import com.beeshroom.Brickery.blocks.plank_fancy_birch;
+import com.beeshroom.Brickery.blocks.plank_fancy_dark_oak;
+import com.beeshroom.Brickery.blocks.plank_fancy_jungle;
+import com.beeshroom.Brickery.blocks.plank_fancy_oak;
+import com.beeshroom.Brickery.blocks.plank_fancy_spruce;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelBlock;
+import net.minecraft.item.EnumDyeColor;
 
 public class ModBlocks 
 {
@@ -37,27 +31,31 @@ public class ModBlocks
 	public static final Block BLOCKCRACKEDBRICKS = new blockcrackedbricks("blockcrackedbricks", Material.ROCK);
 	public static final Block BLOCKMIXEDCOBBLE = new blockmixedcobble("blockmixedcobble", Material.ROCK);
 	public static final Block BLOCK_BRICK_TILE = new block_brick_tile("block_brick_tile", Material.ROCK);
+	public static final Block BLOCK_DIRTY_BRICKS = new block_dirty_bricks("block_dirty_bricks", Material.ROCK);
+	
+	
+	//brick wall???? plz help
+	//public static final Block BLOCK_BRICK_WALL = new ModelBlock ("block_brick_wall");
+	
+	
 	
 	//terracotta
-	public static final Block BLOCK_TERRA_BRICK = new block_terra_brick("block_terra_brick", Material.ROCK);
-	public static final Block BLOCK_RED_TERRA_BRICK = new block_red_terra_brick("block_red_terra_brick", Material.ROCK);
-	public static final Block BLOCK_ORANGE_TERRA_BRICK = new block_orange_terra_brick("block_orange_terra_brick", Material.ROCK);
-	public static final Block BLOCK_YELLOW_TERRA_BRICK = new block_yellow_terra_brick("block_yellow_terra_brick", Material.ROCK);
-	public static final Block BLOCK_LIME_TERRA_BRICK = new block_lime_terra_brick("block_lime_terra_brick", Material.ROCK);
-	public static final Block BLOCK_GREEN_TERRA_BRICK = new block_green_terra_brick("block_green_terra_brick", Material.ROCK);
-	public static final Block BLOCK_CYAN_TERRA_BRICK = new block_cyan_terra_brick("block_cyan_terra_brick", Material.ROCK);
-	public static final Block BLOCK_LIGHT_BLUE__TERRA_BRICK = new block_light_blue_terra_brick("block_light_blue_terra_brick", Material.ROCK);
-	public static final Block BLOCK_BLUE_TERRA_BRICK = new block_blue_terra_brick("block_blue_terra_brick", Material.ROCK);
-	public static final Block BLOCK_PURPLE_TERRA_BRICK = new block_purple_terra_brick("block_purple_terra_brick", Material.ROCK);
-	public static final Block BLOCK_MAGENTA_TERRA_BRICK = new block_magenta_terra_brick("block_magenta_terra_brick", Material.ROCK);
-	public static final Block BLOCK_PINK_TERRA_BRICK = new block_pink_terra_brick("block_pink_terra_brick", Material.ROCK);
-	public static final Block BLOCK_WHITE_TERRA_BRICK = new block_white_terra_brick("block_white_terra_brick", Material.ROCK);
-	public static final Block BLOCK_LIGHT_GRAY_TERRA_BRICK = new block_light_gray_terra_brick("block_light_gray_terra_brick", Material.ROCK);
-	public static final Block BLOCK_GRAY_TERRA_BRICK = new block_gray_terra_brick("block_gray_terra_brick", Material.ROCK);
-	public static final Block BLOCK_BLACK_TERRA_BRICK = new block_black_terra_brick("block_black_terra_brick", Material.ROCK);
-	public static final Block BLOCK_BROWN_TERRA_BRICK = new block_brown_terra_brick("block_brown_terra_brick", Material.ROCK);
+	//public static final Block BLOCTERRACOTTABRICK = new BlockTerracottaBrick("BlockTerracottaBrick", Material.ROCK);
+	public static Block[] TerracottaBrick = new Block[16];
+	{
+	for(EnumDyeColor color : EnumDyeColor.values()) {
+	    TerracottaBrick[color.getMetadata()] = new BlockTerracottaBrick(color);
+	}
+	}
 	
 	
+	//wood planks
+	public static final Block PLANK_FANCY_OAK = new plank_fancy_oak("plank_fancy_oak", Material.WOOD);
+	public static final Block PLANK_FANCY_BIRCH = new plank_fancy_birch("plank_fancy_birch", Material.WOOD);
+	public static final Block PLANK_FANCY_SPRUCE = new plank_fancy_spruce("plank_fancy_spruce", Material.WOOD);
+	public static final Block PLANK_FANCY_JUNGLE = new plank_fancy_jungle("plank_fancy_jungle", Material.WOOD);
+	public static final Block PLANK_FANCY_ACACIA = new plank_fancy_acacia("plank_fancy_acacia", Material.WOOD);
+	public static final Block PLANK_FANCY_DARK_OAK = new plank_fancy_dark_oak("plank_fancy_dark_oak", Material.WOOD);
 	
 	
 	
